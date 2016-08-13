b283da97752c7b411287c643bf39f017fa94095a
kevinrad09 <kevinrademacher@MacBook-Pro.local>
2016-08-13 14:03:57 -0500

    first entry

---
+++ Kevin Rademacher Assessment/orange.js
@@ -0,0 +1,30 @@
+<script   src="https://code.jquery.com/jquery-1.12.4.js"   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="   crossorigin="anonymous"></script>
+        <script>
+            /*$("#slide").hover(function(){
+                $("#slide").slideUp("slow");
+            });
+            
+            $("#slide").hover(function(){
+                $("#slide").slideDown("slow");
+            });
+            */
+            
+            $("#slide").click(function(){
+
+                $("#popup").fadeIn(1000);
+
+            });
+            
+            $("#close").click(function(){
+
+                $("#popup").fadeOut(1000);
+
+            });
+
+			
+
+			
+            
+         
+            
+        </script>
